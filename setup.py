68d181033377a6f0adda9eb9c588e2d9df047e52
DE-PROD\CHSWA <chswa@orsted.dk>
2021-04-28 10:41:02 +0200

    Create setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='mhmm',
+      version='1.0',
+      author='Christopher Warhuus',
+      author_email='christopher.warhuus@gmail.com',
+      packages=['mom'],
+      install_requires=['numpy', 'scipy']
+      )
